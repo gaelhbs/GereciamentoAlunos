bcdacb678d9571a8f20e8861c83183a14cc6be47
gaelhbs <gabrielsouza1170@gmail.com>
2025-03-06 21:28:08 -0300

    Commit inicial

--- src/Curso.java
+++ src/Curso.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Curso {
+    private String nomeCurso;
+    List<Aluno> alunos = new ArrayList<Aluno>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public Curso(String nomeCurso, List<Aluno> alunos) {
+        this.nomeCurso = nomeCurso;
+        this.alunos = alunos;
+    }
+
+    public void adicionarAluno() {
+        System.out.println("Adicione um aluno");
+        String nomeAluno = scanner.nextLine();
+        System.out.println("Digite a matricula do aluno:");
+        String matricula = scanner.nextLine();
+        System.out.println("Digite a nota do aluno:");
+        int nota = scanner.nextInt();
+        scanner.nextLine();
+
+
+        Aluno aluno = new Aluno(nomeAluno,matricula,nota);
+        alunos.add(aluno);
+        System.out.println("Aluno " + nomeAluno + " adicionado!");
+    }
+
+    public void listarAlunos() {
+        if (alunos.isEmpty()){
+            System.out.println("Nenhum aluno cadastrado");
+        }
+        for (Aluno aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
+
+    public  void buscarAlunoPorMatricula() {
+        System.out.println("Digite a matricula do aluno que deseja buscar:");
+        int matricula = scanner.nextInt();
+
+        if (alunos.isEmpty()){
+            System.out.println("Nenhum aluno cadastrado");
+        }
+        for (Aluno aluno : alunos) {
+            if (aluno.getMatricula().equals(matricula)){
+                System.out.println(aluno.toString());
+            }
+        }
+    }
+
+
 }
+
--- src/Sistema.java
+++ src/Sistema.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Sistema {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Curso> listaDeCursos = new ArrayList<>();
+         int opcao = -1;
+
+        while(true) {
+            System.out.println("1 - Cadastro de Cursos");
+            System.out.println("2 - Listar Cursos");
+            System.out.println("3 - Cadastrar um novo aluno em um curso:");
+            System.out.println("4 - Listar todos os alunos de um curso:");
+            System.out.println("5 - Buscar aluno por matrícula");
+            System.out.println("6- Sair.");
+            switch (opcao){
+                case 1:
+
+            }
+        }
+
+    }
 }
